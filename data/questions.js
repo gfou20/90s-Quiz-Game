89bc2c0c9b33c9aac5806ae805bbc87a6380e85e
GFou <gfouchard20@outlook.com>
2022-09-09 16:48:42 -0400

    add questions data file

---
+++ data/questions.js
@@ -0,0 +1,43 @@
+const nickQuestions = [
+  "Which character was the brave leader of the Rugrats?",
+  "What was the name of the dinosaur the Rugrats admired?",
+  "Which show focused on the adolescent life of a teen living in Bluffington?",
+  "Who was the secret love interest of the the main character in Doug?",
+  "Which character was known as 'football head'?",
+  "Which character always made fun of Arnold, but secretly liked him?",
+  "What is the name of the restaurant SpongeBob works at?",
+  "What is the name of Spongebob’s neighbor who always wants to be left alone?",
+]
+
+const toonQuestions = [
+  "What was the accidental ingredient that created the PowerPuff girls?",
+  "Which character used to be friends with Professor Utonium?",
+  "Which “Ed” was the brains in trio Ed, Edd and Eddy?",
+  "What was Ed, Edd and Eddy’s favorite candy?",
+  "What was the name of Dexter’s annoying sister?",
+  "Who was Dexter’s biggest rival?",
+  "Where did Courage, Muriel and Eustace live?",
+  "Which of these characters made an appearance in Courage the Cowardly Dog?",
+]
+
+const disneyQuestions = [
+  "Where do the Gargoyles wake up after being petrified for a thousand years?",
+  " What was the name of the leader of the Gargoyles?",
+  "What was the favorite quote used to describe their lifestyle philosophy of Timon of Pumbaa?",
+  "What animals were Timon and Pumbaa?",
+  "What is the name of Aladdin’s best friend?",
+  "Where did Aladdin find the Genie’s lamp?",
+  "How did Pepper Ann usually show her emotions?",
+  "Pepper Ann was the first Disney animated tv series to :",
+]
+
+const randomQuestions = [
+  "What was the name of Space Ghost’s enemy turned friend?",
+  "Space Ghost went from fighting crime to:",
+  "Which South Park character always comes back after dying?",
+  "Where does South Park take place?",
+  "Which Family Guy character usually fights Peter 'on sight'?",
+  "Which Family Guy character usually gives a “Giggity” as perverse behavior?",
+  "'Bite my shiny metal a**' is a favorite phrase of which Futurama character?",
+  "What year was Fry in when he was cryogenically frozen?",
+]
