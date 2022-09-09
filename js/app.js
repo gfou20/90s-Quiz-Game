882acd5d1b75f7cc00071df76f378a5e91b64308
GFou <gfouchard20@outlook.com>
2022-09-09 17:22:02 -0400

    add cached elements

--- js/app.js
+++ js/app.js
@@ -11,9 +11,16 @@ const answers = []
 
 const score
 
+const timer = 25
+
 /*------------------------ Cached Element References ------------------------*/
 
-const 
+const nickBtnEl = document.getElementById("nick-btn")
+const toonBtnEl = document.getElementById("nick-btn")
+const disneyBtnEl = document.getElementById("nick-btn")
+const randomBtnEl = document.getElementById("nick-btn")
+const resetBtnEl = document.getElementById("nick-btn")
+const mainBtnEL = document.getElementById("nick-btn")
 
 /*----------------------------- Event Listeners -----------------------------*/
 
